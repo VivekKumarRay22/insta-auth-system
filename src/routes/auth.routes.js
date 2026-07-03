eea8fe38b0e4bfe56c37ea416e5357fc57b2da5e
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-07-03 17:40:32 +0530

    login api added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -54,4 +54,46 @@ authRouter.post("/register", async (req, res) => {
   });
 });
 
+authRouter.post("/login", async (req, res) => {
+  const { email, username, password } = req.body;
+  const user = await userModel.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: " user not found",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const isPasswordValid = user.password === hash;
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "invalid password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    messsage: "user logged in successfully",
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+});
+
 module.exports = authRouter;

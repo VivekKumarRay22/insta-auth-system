1871437263d8795b4595549d85d8c0ebfd81d4e7
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-07-03 16:17:28 +0530

    debugged the code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const authRouter = require("./routes/auth.routes");
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser)
+app.use(cookieParser())
 app.use("/api/auth",authRouter)
 
 module.exports = app;

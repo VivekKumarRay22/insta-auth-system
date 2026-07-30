9eff28d1ed9f58e12701fe5def986d24c4b9f85e
VivekKumarRay22 <vivekkumarray855@gmail.com>
2026-07-30 16:15:38 +0530

    createPost page created and styled

--- Frontend/src/features/posts/pages/CreatePost.jsx
+++ Frontend/src/features/posts/pages/CreatePost.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import "../style/createpost.scss"
+
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
+    return (
+        <main className="create-post-page">
+            <div className="form-container">
+                <h1>Create Post</h1>
+                <form>
+                    <label className='create-post-label' htmlFor="postImage">Select Image</label>
+                    <input hidden type="file" name='postImage' id='postImage' />
+                    <input type="text" name="caption" id="caption" />
+                    <button className='button primary-button'>Create Post</button>
+                </form>
+            </div>
+        </main>
+    )
 }
 
 export default CreatePost
